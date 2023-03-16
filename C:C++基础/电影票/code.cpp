1fdaed5210e64907997f723b4cd47064d687a872
zhangfenglin <zfl1129@gmail.com>
2023-03-16 23:17:05 +0800

    feat: 电影票解题

---
+++ C:C++基础/电影票/code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int price = 10;
+
+    int x;
+    cin >> x;
+
+    int total = x * price;
+
+    cout << x << " " << total;
+    
+    return 0;
+}
