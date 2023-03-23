77dd921b40d2e9c2ff6202da1911620bdaa422e3
zhangfenglin <zfl1129@gmail.com>
2023-03-23 23:07:33 +0800

    feat: 交换值

---
+++ C++基础/交换值/code.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b;
+    c = a;
+    a = b;
+    b = c;
+    cout << a << " " << b;
+    return 0;
+}
