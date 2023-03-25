ab7fff171bd1e6d99a9a80ec9a91d92cbea43448
zhangfenglin <zfl1129@gmail.com>
2023-03-25 23:17:42 +0800

    feat: 计算存储空间大小

---
+++ C++基础/整型数据类型存储空间大小/code.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    short b;
+    printf("%ld %ld", sizeof(a),sizeof(b));
+    return 0;
+}
---
+++ C++基础/浮点型数据类型存储空间大小/code.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float a;
+    double b;
+    printf("%ld %ld", sizeof(a),sizeof(b));
+    return 0;
+}
