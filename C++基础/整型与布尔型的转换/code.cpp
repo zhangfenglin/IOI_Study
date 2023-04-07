82526ebda8661643cfc52f5857d57d505a182384
zhangfenglin <zfl1129@gmail.com>
2023-04-07 23:21:05 +0800

    feat: 整型与布尔型的转换

---
+++ C++基础/整型与布尔型的转换/code.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+    int a;
+
+    scanf("%d", &a);
+
+    bool b = a;
+
+    int c = b;
+
+    printf("%d", c);
+
+    return 0;
+}
---
+++ C++基础/整型与布尔型的转换/desc.md
@@ -0,0 +1,1 @@
+http://ybt.ssoier.cn:8088/problem_show.php?pid=1022
